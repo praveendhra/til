83ccf03b77780579e8fccb027bc6bf56e49d711d
Praveendhra Rajkumar <prvn9021@gmail.com>
2026-05-29 19:58:47 -0400

    Refactor ListNode class definition for clarity and formatting

--- google-prep/leetcode/Add_two_Numbers/Solution.java
+++ google-prep/leetcode/Add_two_Numbers/Solution.java
@@ -1,14 +1,16 @@
 /**
  * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+*/
+
+ 
 class Solution {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         // dummy is a throwaway head node so we never need special logic for the first node.
         // curr is our pointer that we advance as we build the result list.
